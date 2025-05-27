76059144e310cf8602154e697a35948d7716589a
Surennddhar <surendharjeyakumar@gmail.com>
2025-05-27 12:22:17 +0530

    pagination

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -2,25 +2,25 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL :"https://jsonplaceholder.typicode.com"
+    baseURL: "https://jsonplaceholder.typicode.com"
 });
 
 // to fetch the data
-export const fetchPosts = async() => {
-    const res = await  api.get("/posts");
-      return   res.status ===200 ? res.data :[];
+export const fetchPosts = async (pageNumber) => {
+    const res = await api.get(`/posts?_start=${pageNumber}&_limit=3`);
+    return res.status === 200 ? res.data : [];
 }
 
 // to fetch the indivial data
 
-export const fetchIndvPost = async(id) => {
-    try{
-     const res = await  api.get(`/posts/${id}`)
-     return res.status === 200 ? res.data:[];
+export const fetchIndvPost = async (id) => {
+    try {
+        const res = await api.get(`/posts/${id}`)
+        return res.status === 200 ? res.data : [];
     }
-    catch(error)
-    {
-     console.log(error);
+    catch (error) {
+        console.log(error);
     }
-
 }
+// ..pagination
+//! https://jsonplaceholder.typicode.com/posts?_start=1&_limit=3
--- src/Pages/FetchRQ.jsx
+++ src/Pages/FetchRQ.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { fetchPosts } from '../API/api';
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { NavLink } from 'react-router-dom';
 
 const FetchRQ = () => {
 
   // react query 
-
+  const [pageNumber, setPageNumber] = useState(0);
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ["posts"],  // useState
-    queryFn: fetchPosts,  //useEfect
+    queryKey: ["posts", pageNumber],  // useState
+    queryFn: () => fetchPosts(pageNumber),  //useEfect
     // gcTime:1000,
     // staleTime:10000,
-    refetchInterval: 1000,
-    refetchIntervalInBackground: true,
+    // refetchInterval: 1000,
+    // refetchIntervalInBackground: true,
+    placeholderData: keepPreviousData,
   });
   // Conditional rendering based on loading, error, and posts data
   if (isPending) return <p>Loading...</p>;
   if (isError) return <p>Error : {error.message || "Something went wrong!"}</p>;
-
   return (
     <div>
       <ul className="section-accordion">
@@ -36,6 +36,19 @@ const FetchRQ = () => {
           );
         })}
       </ul>
+      <div className="pagination-section container">
+        <button 
+        disabled = {pageNumber === 0 ? true : false }
+        onClick={() => setPageNumber((prev) => prev - 3)}>
+          Prev
+        </button>
+        <p>
+          {(pageNumber / 3 + 1)}
+        </p>
+        <button onClick={() => setPageNumber((prev) => prev + 3)}>
+          Next
+        </button>
+      </div>
     </div>
   )
 }
